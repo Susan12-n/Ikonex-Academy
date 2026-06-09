16b229e0c17de6208208ae117d53bdaeb1c1265c
Susan12-n <ngesasuzan@gmail.com>
2026-06-09 10:23:20 +0300

    editted the backend url

--- frontend/Ikonex-frontend/src/pages/Login.jsx
+++ frontend/Ikonex-frontend/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
 
+
 function Login() {
   const navigate = useNavigate();
 
--- frontend/Ikonex-frontend/src/pages/Register.jsx
+++ frontend/Ikonex-frontend/src/pages/Register.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
+
+
 function Register() {
   const navigate = useNavigate();
 
